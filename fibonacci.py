24b67712e47cd05fc5353c5efa317774dcc6bc17
Gennaquach <60235199+Gennaquach@users.noreply.github.com>
2022-03-26 01:06:08 -0700

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+def fibonacci(n):
+         
+    # If n = 0 returns 0
+    if n == 0:
+        return 0
+       
+    # If n = 1 returns 1
+    elif n == 1:
+        return 1
+
+    # If n >= 2 returns the term of sequence
+    else:
+        term = fibonacci(n-1) + fibonacci(n-2)
+        return term
+# Input for 15th term
+input_n = input("Please enter a positive integer n to find the fibonacci term for: ")
+
+if input_n.strip().isdigit() and int(input_n) >= 0:
+    n = int(input_n)
+    print("The n value entered is:", n, "\nThe fibonacci term for that is:", fibonacci(n))
+
+else:
+    print("That is not a valid positive integer, please try again.")
+
